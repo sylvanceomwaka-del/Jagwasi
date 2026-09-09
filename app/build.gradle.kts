2f69286a473fbb97cd661eabab90675d8f367a22
YourName <youremail@example.com>
2026-09-10 00:45:11 +0300

    Add Hilt, security-crypto dependencies and remove duplicate interface

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
+    id("com.google.dagger.hilt.android")
+
+
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
@@ -37,6 +40,17 @@ android {
 }
 
 dependencies {
+    // Hilt
+
+    implementation("com.google.dagger:hilt-android:2.51.1")
+
+    kapt("com.google.dagger:hilt-compiler:2.51.1")
+
+    // Security Crypto
+
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
+
+
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.activity:activity-compose:1.9.1")
